3e257af2e265b64f5818a0aed5c480557cc39b36
Alex <hlin3517@gmail.com>
2021-08-12 14:53:04 -0400

    fourth attempt

--- pages/api/questions.js
+++ pages/api/questions.js
@@ -28,12 +28,8 @@ export async function getQuestionDetail(questionUrl = "") {
     const context = await browser.newContext()
     const page = await context.newPage()
 
-    // get daily question
     if (!questionUrl) {
-      //Let page goto leetcode website
       await page.goto("https://leetcode.com/problemset/all/")
-      //The first row of the rowgroup is today's question
-      //In future, we only need to change the css selector
       const textContent = await page.textContent(
         `div[role="rowgroup"] > div:first-child > div > div a[href*="/explore/item"]`
       )
@@ -44,48 +40,24 @@ export async function getQuestionDetail(questionUrl = "") {
 
     console.log("12312312", questionUrl)
 
-    //Let page goto leetcode problem
     await page.goto(questionUrl)
-    //In future, we only need to change the css selector
 
-    //=====================================================================================================
     const questionTitle = await page.textContent(
       `div[data-key*=description-content] > div div[data-cy=question-title]`
     )
-    console.log(questionTitle)
-    //=====================================================================================================
 
-    //=====================================================================================================
     const diff = await page.textContent(`div[data-key*=description-content] > div div[diff]`)
-    console.log(diff)
-    //=====================================================================================================
 
-    //=====================================================================================================
-    // const questionContentText = await page.innerText(
-    //   `div[data-key*=description-content] > div div[class*=question-content]`
-    // );
-    // console.log(questionContentText);
-    //=====================================================================================================
-
-    //=====================================================================================================
     const questionContentHTML = await page.innerHTML(
       `div[data-key*=description-content] > div div[class*=question-content]`
     )
-    console.log(questionContentHTML)
-    //=====================================================================================================
-
-    //=====================================================================================================
     const tagPromises = await page.$$(`div > a[class*=topic-tag]`)
     let tags = []
-    // tagPromises.forEach(async (e) => {
-    //   tags.push(await e.innerText());
-    // });
 
     for (const tagPromise of tagPromises) {
       tags.push(await tagPromise.innerText())
     }
     console.log(tags)
-    //=====================================================================================================
 
     await page.close()
     await context.close()
